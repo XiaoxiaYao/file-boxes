5fea8bd18aeabb92954e4f39b9870f9d454c12cd
MichaelYao <michaelyaonz@gmail.com>
2022-04-28 17:52:03 +0800

    Connect development mongodb.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -11,7 +12,15 @@ import * as Joi from 'joi';
       envFilePath: `.env.${process.env.NODE_ENV}`,
       validationSchema: Joi.object({
         PORT: Joi.number().default(4000),
+        MONGODB_URI: Joi.string().required(),
       }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
     AuthModule,
