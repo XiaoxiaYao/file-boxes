41ab9717eebc718f22bef2aec9c4e1302786efd1
MichaelYao <michaelyaonz@gmail.com>
2022-04-28 16:38:47 +0800

    Use Config for PORT for different environment.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(4000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
