ca134824f6d5b09760f582425f08938545841658
MichaelYao <michaelyaonz@gmail.com>
2022-05-03 00:38:44 +0800

    Enable cors.

--- client/src/Api.js
+++ client/src/Api.js
@@ -4,46 +4,46 @@ axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 axios.defaults.withCredentials = true;
 
 export const signup = ({ email, password }) =>
-  axios.post('/auth/sign-up/', {
+  axios.post('/api/auth/sign-up/', {
     email,
     password,
   });
 
 export const signin = ({ email, password }) =>
-  axios.post('/auth/sign-in/', {
+  axios.post('/api/auth/sign-in/', {
     email,
     password,
   });
 
-export const signout = () => axios.post('/auth/sign-out/');
+export const signout = () => axios.post('/api/auth/sign-out/');
 
-export const currentUser = () => axios.get('/auth/current-user/');
+export const currentUser = () => axios.get('/api/auth/current-user/');
 
-export const listBoxes = () => axios.get('/boxes/');
+export const listBoxes = () => axios.get('/api/boxes/');
 
-export const retrieveBox = (boxId) => axios.get(`/boxes/${boxId}`);
+export const retrieveBox = (boxId) => axios.get(`/api/boxes/${boxId}`);
 
 export const createBox = ({ name, description }) =>
-  axios.post('/boxes/', { name, description });
+  axios.post('/api/boxes/', { name, description });
 
 export const updateBox = ({ boxId, name, description }) =>
-  axios.patch(`/boxes/${boxId}/`, { name, description });
+  axios.patch(`/api/boxes/${boxId}/`, { name, description });
 
-export const deleteBox = (boxId) => axios.delete(`/boxes/${boxId}`);
+export const deleteBox = (boxId) => axios.delete(`/api/boxes/${boxId}`);
 
 export const uploadFile = (boxId, formData) =>
-  axios.post(`/boxes/${boxId}/upload-file`, formData, {
+  axios.post(`/api/boxes/${boxId}/upload-file`, formData, {
     headers: {
       'content-type': 'multipart/form-data',
     },
   });
 
 export const setToPublic = (boxId) =>
-  axios.patch(`/boxes/${boxId}/set-to-public`);
+  axios.patch(`/api/boxes/${boxId}/set-to-public`);
 
 export const shareBox = ({ boxId, email }) =>
-  axios.patch(`/boxes/${boxId}/share`, { email });
+  axios.patch(`/api/boxes/${boxId}/share`, { email });
 
-export const listUsers = () => axios.get('/users/');
+export const listUsers = () => axios.get('/api/users/');
 
-export const deleteUser = (userId) => axios.delete(`/users/${userId}`);
+export const deleteUser = (userId) => axios.delete(`/api/users/${userId}`);
--- server/src/main.ts
+++ server/src/main.ts
@@ -12,6 +12,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('/api');
   // Global Validation
   app.useGlobalPipes(
     new ValidationPipe({
